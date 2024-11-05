1cc4f49fdbc2735a0f10e2893eed9fd03c525dcc
“Mauro-pixel” <espinal831@gmail.com>
2024-11-04 19:50:35 -0500

    Se retira los comentarios

--- src/main/java/com/proyectos/transicionenergetica/TipoEnergia.java
+++ src/main/java/com/proyectos/transicionenergetica/TipoEnergia.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.proyectos.transicionenergetica;
 
-/**
- *
- * @author mauro
- */
+
 public class TipoEnergia  extends EnergiaRenovable{
     
     public String nombreTipoEnergia;
--- src/main/java/com/proyectos/transicionenergetica/TransicionEnergetica.java
+++ src/main/java/com/proyectos/transicionenergetica/TransicionEnergetica.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.proyectos.transicionenergetica;
 
-/**
- *
- * @author mauro
- */
 public class TransicionEnergetica {
 
     public static void main(String[] args) {
